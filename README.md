a50edcb3ef4595b7f97814223e914311bfaa023c
Elena <bohatchuk.o@gmail.com>
2022-06-04 02:38:57 +0300

    upd files

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # IT Hillel course Python Pro 05.2022
-
-<span style="color:violet">about course </span>
---
+++ lesson_3_code_quality/classwork/exaples.py
@@ -0,0 +1,14 @@
+def ndfjkbnnhhhhhknsvbbn(num1:int,
+                        num2:str,
+                        num3:int,
+                        num4:str) -> None:
+    pass
+
+
+def ndfjkbnnhhhhhknsvbbn(
+    num1:int,
+    num2:str,
+    num3:int,
+    num4:str
+) ->None:
+    pass
