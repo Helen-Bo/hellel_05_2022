d33f80fbb6eaaabf64d6cc8992b7794b6fc1afe1
Elena <bohatchuk.o@gmail.com>
2022-06-07 15:42:13 +0300

    update file

--- lesson_1/classwork/lesson1.py
+++ lesson_1/classwork/lesson1.py
@@ -5,22 +5,27 @@
 class A:
     def foo(self):
         print("I am in A")
-        
+
+
 class B(A):
     def foo(self):
         print("I am in B")
-        
+
+
 class C(A):
     def foo(self):
         print("I am in C")
-        
+
+
 class H(A):
     pass
+
 
 class D(B, C, H):
     def foo(self):
         super(C, self).foo()
-    
+
+
 D().foo()
 
 D.__mro__
---
+++ lesson_3_code_quality/classwork/examples.py
@@ -0,0 +1,23 @@
+# yapf
+def foosdasdasdkasdajdsdasdadasdas(
+    number_1: int,
+    number_2: str,
+    number_3: str,
+    number_4: str,
+) -> None:
+    pass
+
+
+# black
+def foosdasdasdkasdajdsdasdadasdas2(
+    number_1: int,
+    number_2: str,
+    number_3: int,
+    number_4: str,
+    number_5: str,
+) -> None:
+    pass
+
+
+def bar(arg_1):
+    return 4
